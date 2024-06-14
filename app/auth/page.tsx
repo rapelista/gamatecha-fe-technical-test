64612cfb1d553fffea96f1b288ff2476da889045
Farih Akmal Haqiqi <farihakmal@student.ub.ac.id>
2024-06-14 20:29:40 +0700

    feat: toggle theme mode on auth page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,5 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { ModeToggle } from "@/components/theme-mode-toggle";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,8 +8,11 @@ export const metadata: Metadata = {
 
 export default function AuthPage() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="flex items-center justify-center h-screen gap-2">
             <LoginForm />
+            <div className="absolute bottom-2 sm:bottom-4 right-2 sm:right-4">
+                <ModeToggle />
+            </div>
         </div>
     );
 }
