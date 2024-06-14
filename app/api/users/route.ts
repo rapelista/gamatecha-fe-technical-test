528e1f21c226e665150f7c80dec4ba8cc7af54b2
Farih Akmal Haqiqi <farihakmal@student.ub.ac.id>
2024-06-14 23:27:14 +0700

    feat: add route handler

---
+++ app/api/users/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+import dummyUsers from "@/lib/dummyUsers.json";
+import UserType from "entities/User";
+
+export async function GET(request: NextRequest) {
+    const users = dummyUsers as UserType[];
+    return NextResponse.json(users);
+}
